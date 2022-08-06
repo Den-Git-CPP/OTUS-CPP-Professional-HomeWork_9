ff5cae2ef0301b85f9d536b7658c50aadf49ab0e
Den-Git-CPP <madim-neodart@rambler.ru>
2022-08-06 12:39:23 +0300

    добалвены async h cpp

--- source/bulk.cpp
+++ source/bulk.cpp
@@ -1,28 +1,19 @@
 ﻿#include <iostream>
-#include <sstream>
-#include "Logger_in_Console.h"
-#include "Logger_in_File.h"
-#include "Accumulator_Commands.h"
 
-int main(int argc, char** argv) {
-	int number_commands_in_pull{0};
-	if(argc <= 1) {
-		std::cout << "Incorrect number of arguments";
-	}
-	else {
-		try {
-			number_commands_in_pull = std::stoi(argv[1]);
-			if(number_commands_in_pull <= 0) {
-				std::cout << "Number of commands in bulk must be positive";
-			}
-		}
-		catch(const std::exception& error_command) {
-			std::cerr << error_command.what();
-		}
-	}
+#include "async.h"
 
-	Accumulator_Commands accumulator(number_commands_in_pull);
-	Logger_in_Console log_in_console(&accumulator);
-	Logger_in_File log_in_file(&accumulator);
-	accumulator.work_with_commands();
+int main(int, char *[]) {
+    std::size_t bulk = 5;
+    auto h = async::connect(bulk);
+    auto h2 = async::connect(bulk);
+        
+    async::receive(h, "1", 1);
+    async::receive(h2, "1\n", 2);
+    async::receive(h, "\n2\n3\n4\n5\n6\n{\na\n", 15);
+    async::receive(h, "b\nc\nd\n}\n89\n", 11);
+    
+    async::disconnect(h);
+    async::disconnect(h2);
+
+    return 0;
 }
