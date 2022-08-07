0b66a0c6c4b8f6a99f16ae2339edbc34d8779d3a
Den-Git-CPP <madim-neodart@rambler.ru>
2022-08-07 07:32:20 +0300

    Переработан PoolCmd

--- source/PoolCmd.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "PoolCmd.h"
-void PoolCmd::push(const std::string& cmd) {
-  if(pool_.empty())
-    start_bulk_time_ = std::time(nullptr);
-  if(!cmd.empty())
-    pool_.emplace_back(cmd);
-}
-
-
