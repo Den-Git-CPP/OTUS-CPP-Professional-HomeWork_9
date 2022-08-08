c1610f6426e85c889a63769973e2edf44fa0fd32
Den-Git-CPP <madim-neodart@rambler.ru>
2022-08-08 06:09:40 +0300

    Подключены тесты к Actions

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,61 @@
-#pragma once
-#include  "g_test_bulk.h"
+#include "gtest/gtest.h"
+#include "../lib/ContextCmd.h"
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+class TestWriter : public StreamWriter {
+  public:
+    TestWriter() {
+      bulk_pool_.clear();
+      metrics_.clear();
+      time_ = std::time_t{};
+      context_id_ = uint8_t{};
+    }
+
+    ~TestWriter() override = default;
+
+    void write(uint8_t context_id, const Bulk& bulk) final {
+      bulk_pool_ = bulk.get_cmds();
+      time_ = bulk.time();
+      metrics_.push(std::this_thread::get_id(), bulk);
+      context_id_ = context_id;
+    }
+
+    static auto get_bulk() {
+      return bulk_pool_;
+    }
+
+    static auto get_time() {
+      return time_;
+    }
+
+    Metrics& get_metrics() final {
+      return metrics_;
+    }
+
+    static Metrics& get_metrics_stub() {
+      return metrics_;
+    }
+
+    static auto get_context_id() {
+      return time_;
+    }
+
+  private:
+    static std::vector<std::string> bulk_pool_;
+    static std::time_t time_;
+    static Metrics metrics_;
+    static uint8_t context_id_;
+};
+
+std::vector<std::string> TestWriter::bulk_pool_{};
+std::time_t TestWriter::time_{};
+Metrics TestWriter::metrics_{"test"};
+uint8_t TestWriter::context_id_{};
+
+
 
 TEST(context_test_case, subscribe_test) {
  ContextCmd context{1, 1};
@@ -23,7 +79,7 @@ TEST(context_test_case, partial_bulk_test) {
   std::string str = "cmd1\ncmd2\n";
 
   context.process(str.c_str(), str.size());
-  context.process(nullptr, 0, true);  
+  context.process(nullptr, 0, true); // Принудительное завершение блока.
 
   std::vector<std::string> result{"cmd1", "cmd2"};
   EXPECT_EQ(TestWriter::get_bulk(), result);
@@ -52,7 +108,7 @@ TEST(context_test_case, full_tail_bulk_test) {
   std::string str = "cmd1\ncmd2\ncmd3\ncmd4\ncmd5\n";
 
   context.process(str.c_str(), str.size());
-  context.process(nullptr, 0, true);  
+  context.process(nullptr, 0, true); // Принудительное завершение блока.
 
   std::vector<std::string> result{"cmd4", "cmd5"};
   EXPECT_EQ(TestWriter::get_bulk(), result);
@@ -109,7 +165,7 @@ TEST(context_test_case, eof_bulk_test) {
   std::string str = "cmd1\ncmd2\n{\ncmd3\ncmd4\ncmd5\ncmd6\n}\ncmd7\ncmd8\n";
 
   context.process(str.c_str(), str.size());
-  context.process(nullptr, 0, true);  
+  context.process(nullptr, 0, true); // Принудительное завершение блока.
 
   std::vector<std::string> result{"cmd7", "cmd8"};
   EXPECT_EQ(TestWriter::get_bulk(), result);
@@ -124,7 +180,8 @@ TEST(context_test_case, metrics_test) {
   std::string str = "cmd1\ncmd2\n{\ncmd3\ncmd4\ncmd5\ncmd6\n}\ncmd7\ncmd8\n";
 
   context.process(str.c_str(), str.size());
-  context.process(nullptr, 0, true);  
+  context.process(nullptr, 0, true); // Принудительное завершение блока.
+
   std::stringstream ss1;
   ss1 << "test";
   ss1 << " thread_id " << std::this_thread::get_id() << " - ";
@@ -161,4 +218,9 @@ TEST(bulk_test_case, print_bulk_test) {
   std::string result{"bulk: cmd1, cmd2, cmd3\n"};
 
   EXPECT_EQ(ss.str(), result);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
